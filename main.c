bcd1b404ff9cec436afef48280288eb4b5cefd8a
luizgaf <luiz.guilhermeaf21@gmail.com>
2024-09-17 10:01:22 -0300

    1

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    char nome[20];
+    char lexema[100];
+    int linha;
+    int coluna;
+} Token;
+
+Token getToken(){
+    char c = nextChar();
+    while (c != EOF){
+        switch(estado){
+            
+        }
+    }
+}
+
+int main(){
+
+
+}
